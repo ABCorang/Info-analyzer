600de8933d3db437a8bb236b7c5f901f38d7ba98
user <172138601+ABCorang@users.noreply.github.com>
2024-08-28 17:37:46 +0900

    Update: import修正

--- pages/youtube_summarizer.py
+++ pages/youtube_summarizer.py
@@ -2,7 +2,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import YoutubeLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import GoogleGenerativeAI, HarmCategory, HarmBlockThreshold
-from langchain_community import YoutubeLoader
+from langchain_community.document_loaders import YoutubeLoader
 from pytube import YouTube
 import pytube.exceptions as pytube_exceptions
 
