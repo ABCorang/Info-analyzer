b40a6fa5ac1b0bb95616d5fc934333d932602789
user <172138601+ABCorang@users.noreply.github.com>
2024-10-13 05:14:14 +0900

    app.py:積立機能追加

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ import yfinance as yf
 
 default_start_date = datetime.datetime.now() - datetime.timedelta(days=365)
 
-def get_data(ticker, start_date=default_start_date):
-    # df = pdr.DataReader(ticker, data_source='stooq', start=start_d).sort_index()
-    df = yf.download(tickers=ticker, start=start_date)
+def get_data(ticker, start_date=None):
+
+    if start_date is None:
+        df = yf.download(tickers=ticker)
+    else:
+        df = yf.download(tickers=ticker, start=start_date)
     return df
 
 def calculate_year_to_date_return(df):
@@ -34,6 +37,24 @@ def calculate_year_to_date_return(df):
 
     return year_to_date_return
 
+
+def calculate_investment_results(ticker, df, investment_amount=1000):
+    """積立投資と一括投資の結果を計算する関数。"""
+    investment_df = df.copy() 
+    investment_df['periodic_investment'] = investment_amount# 定期投資額
+    investment_df['cumulative_investment'] = investment_df['periodic_investment'].cumsum()# 累積投資額
+    investment_df['cumulative_units'] = (investment_df['periodic_investment'] / investment_df[f'{ticker}_jpy']).cumsum()# 累積投資単位数
+
+
+    # 一括買い
+    lump_sum_result = (investment_df['cumulative_investment'].iloc[-1] / investment_df[f'{ticker}_jpy'].iloc[0]) * investment_df[f'{ticker}_jpy'].iloc[-1]
+    # 積立買い
+    dollar_cost_averaging_result = investment_df[f'{ticker}_jpy'].iloc[-1] * investment_df['cumulative_units'].iloc[-1]
+
+    return lump_sum_result, dollar_cost_averaging_result
+
+
+
 def main():
 
     with st.sidebar:
@@ -43,16 +64,21 @@ def main():
             ['^SPX', 'TECL', '^DJI', '^IXIC']
         )
 
+        #リンク
         cnn_link = 'https://edition.cnn.com/markets/fear-and-greed'
         st.markdown(f'links <br>[fear and greed]({cnn_link})', unsafe_allow_html=True)
 
+        st.markdown('毎日積立シミュレーション')
+        selected_num = st.slider('毎日積み立てる額を選択', 0, 10**4, 1000)
+        st.write(selected_num,'(円)')
 
+ 
     input_start_date  = st.date_input(
         "When\'s sart date",
         value=default_start_date
     )
 
-    st.write('start date:', default_start_date)
+    st.write('start date:', input_start_date)
 
     input_ticker = st.text_input('please input ticker:')
 
@@ -61,7 +87,8 @@ def main():
 
     if raw_ticker:
         ticker = unicodedata.normalize('NFKC', raw_ticker)
-        ticker_df = get_data(ticker)
+        ticker_df = get_data(ticker, start_date=input_start_date)
+
 
         target_days = pd.date_range(start=ticker_df.index[0], end=ticker_df.index[-1], freq='D')
         
@@ -101,6 +128,19 @@ def main():
         # 対象tickerの価格データ（新しい順）に表示
         sorted_ticker_df = ticker_df.sort_values(by='Date', ascending=False)
         st.write(sorted_ticker_df)
+
+        st.markdown('---')
+        # ドル円データを取得(円換算用
+        usdjpy = yf.download("JPY=X", start=input_start_date)
+        combined_df = pd.concat([ticker_df['Adj Close'], usdjpy['Adj Close']], axis=1)
+        combined_df.columns = [f'{ticker}', 'usdjpy']
+        combined_df = combined_df.dropna()
+        combined_df[f'{ticker}_jpy'] = combined_df[f'{ticker}'] * combined_df['usdjpy']
+        # 投資結果を計算
+        lump_sum_result, dollar_cost_averaging_result = calculate_investment_results(ticker, combined_df, investment_amount=selected_num)
+        st.write(f"一括投資結果: {lump_sum_result:,.0f}円")
+        st.write(f"積立投資結果: {dollar_cost_averaging_result:,.0f}円")
+        st.write(combined_df)
 
 
 if __name__ == "__main__":
